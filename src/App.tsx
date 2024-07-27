228209ef9586e23f3577ec639b2ad53f9f884595
sh.tashakori <sh.tashakori@wallex.net>
2024-07-27 18:38:56 +0330

    [ADD] basic recursion with data

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  ChevronRightIcon,
+  DocumentIcon,
+  FolderIcon,
+} from '@heroicons/react/24/solid';
+import { useState } from 'react';
+
+type FolderT = {
+  name: string;
+  folders?: FolderT[];
+};
+
+const folders: FolderT[] = [
+  {
+    name: 'Home',
+    folders: [
+      {
+        name: 'Movies',
+        folders: [
+          {
+            name: 'Action',
+            folders: [
+              {
+                name: '2000s',
+                folders: [
+                  { name: 'Gladiator.mp4' },
+                  { name: 'American-Beauty.mp4' },
+                ],
+              },
+              { name: '2010s', folders: [] },
+            ],
+          },
+          { name: 'Comedy', folders: [{ name: '2000s', folders: [] }] },
+        ],
+      },
+      {
+        name: 'Music',
+        folders: [
+          { name: 'Rock', folders: [] },
+          { name: 'Jazz', folders: [] },
+        ],
+      },
+      { name: 'Pictures', folders: [] },
+      { name: 'passwords.txt' },
+    ],
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='p-8 max-w-sm mx-auto'>
+      <ul>
+        {folders.map((folder) => (
+          <Folder key={folder.name} folder={folder} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+const Folder = ({ folder }: { folder: FolderT }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen((isOpen) => !isOpen);
+
+  return (
+    <li className='my-1.5' key={folder.name}>
+      <span className='flex items-center gap-1.5'>
+        {folder.folders ? (
+          <>
+            {folder.folders.length > 0 && (
+              <button onClick={toggle}>
+                <ChevronRightIcon className='size-4 text-gray-500' />
+              </button>
+            )}
+            <FolderIcon className='size-6 text-sky-500' />
+          </>
+        ) : (
+          <DocumentIcon className='size-6 text-gray-700' />
+        )}
+        {folder.name}
+      </span>
+      {isOpen && (
+        <ul className='ps-6'>
+          {folder.folders?.map((folder) => (
+            <Folder key={folder.name} folder={folder} />
+          ))}
+        </ul>
+      )}
+    </li>
+  );
+};
